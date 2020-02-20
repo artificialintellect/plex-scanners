d757127390687a205061c105d9c4639062c2ed3a
artificialintellect <artificialintellect@users.noreply.github.com>
2020-02-20 00:07:18 -0600

    Update PBS Kids Scanner.py

--- PBS Kids Scanner.py
+++ PBS Kids Scanner.py
@@ -341,7 +341,31 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
   # Stack the results.
   if shouldStack:
     Stack.Scan(path, files, mediaList, subdirs)
-  
+
+
+
+# START OF MY EDITS
+  for m in mediaList:
+    if show_applies(m.show):
+      m.episode = (m.episode * 2) - 1
+
+def show_applies(show):
+  return (show == "Nature Cat"
+          or show == "The Cat in the Hat Knows a Lot About That"
+          or show == "Daniel Tiger's Neighborhood"
+          or show == "Dinosaur Train"
+          or show == "Pinkalicious & Peterrific"
+          or show == "Splash and Bubbles"
+          or show == "Curious George"
+          or show == "Molly of Denali"
+          or show == "Xavier Riddle and the Secret Museum")
+# END OF MY EDITS
+
+
+
+
+
+
 def find_data(atom, name):
   child = atomsearch.find_path(atom, name)
   data_atom = child.find('data')
